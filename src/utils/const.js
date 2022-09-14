276a0582da79e12aee8d42a95aede825fe2e8c1a
Suraj Singla <suraj.singla@mckinleyrice.co>
2022-09-14 12:46:47 +0530

    initial setup

---
+++ src/utils/const.js
@@ -0,0 +1,13 @@
+module.exports = {
+    MAINNET_CHAIN_ID: 137,
+    SWAP_ROUTER_ADDRESS: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff',
+    ETHEREUM_ADDRESS: '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE',
+    INFURA_RPC: 'https://polygon-rpc.com/',
+    ERROR_MESSAGES: {
+        NULL_ROUTE: 'Route cannot be null',
+        INVARIANT_ADDRESS: 'Invariant failed: ADDRESSES',
+        QUOTE_OF_NULL: "Cannot read property 'quote' of null",
+        TOKEN_PAIR_DOESNOT_EXIST: "Provided token pair is not supported",
+        INSUFFICIENT_BALANCE: "Insufficient balance."
+    }
+}
